1f612105b22e0b67358efb96cbdfc7b7483cc2c6
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-09-15 12:25:49 -0400

    Update make-multi-filter.js

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,1 +1,20 @@
+function MakeMultiFilter(originalArray) {
+  let currentArray = [...originalArray];
+  
+  function arrayFilterer(filterCriteria, callback) {
+    if (typeof filterCriteria !== 'function') {
+      return currentArray;
+    }
+
+    currentArray = currentArray.filter(filterCriteria);
+
+    if (typeof callback === 'function') {
+      callback(currentArray);
+    }
+
+    return arrayFilterer; // Return itself for chaining
+  }
+
+  return arrayFilterer;
+}
 
